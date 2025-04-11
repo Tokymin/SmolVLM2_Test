53575ad7b79e247519cc33b166bb7dea06ee420d
Tokymin <466350259@qq.com>
2025-04-11 15:15:49 +0800

    加入了smollm git仓库的内容

--- main.py
+++ main.py
@@ -8,27 +8,27 @@ model = AutoModelForImageTextToText.from_pretrained(
     device_map="cuda"
 )
 # 图像的
-# conversation = [
-#     {
-#         "role": "user",
-#         "content":[
-#             {"type": "image", "url": "http://images.cocodataset.org/val2017/000000039769.jpg"},
-#             {"type": "text", "text": "Describe this image."}
-#         ]
-#     }
-# ]
-#
-# inputs = processor.apply_chat_template(
-#     conversation,
-#     add_generation_prompt=True,
-#     tokenize=True,
-#     return_dict=True,
-#     return_tensors="pt",
-# ).to(model.device, dtype=torch.bfloat16)
-#
-# output_ids = model.generate(**inputs, max_new_tokens=128)
-# generated_texts = processor.batch_decode(output_ids, skip_special_tokens=True)
-# print(generated_texts)
+conversation = [
+    {
+        "role": "user",
+        "content":[
+            {"type": "image", "path": "1671158065375.jpg"},
+            {"type": "text", "text": "Describe this image."}
+        ]
+    }
+]
+
+inputs = processor.apply_chat_template(
+    conversation,
+    add_generation_prompt=True,
+    tokenize=True,
+    return_dict=True,
+    return_tensors="pt",
+).to(model.device, dtype=torch.bfloat16)
+
+output_ids = model.generate(**inputs, max_new_tokens=128)
+generated_texts = processor.batch_decode(output_ids, skip_special_tokens=True)
+print(generated_texts)
 
 
 # 视频
